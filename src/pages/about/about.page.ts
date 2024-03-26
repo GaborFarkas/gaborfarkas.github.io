1295b73bc66c47e83a37869e869933450a26ddc4
GaborFarkas <raandal73@gmail.com>
2024-03-26 19:49:37 +0100

    Wording, typos in content

--- src/pages/about/about.page.ts
+++ src/pages/about/about.page.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FaqBlockComponent } from '../../components/faq/faq-block/faq-block.component';
-import { FaqSectionComponent } from '../../components/faq/faq-section/faq-section.component';
 import { FaqGroupModel } from '../../models/faq.model';
 import { CommonModule } from '@angular/common';
 
@@ -24,7 +23,37 @@ export class AboutPage {
                 },
                 {
                     question: ' did you create this website?',
-                    answer: 'To put myself up on the map, to have a presence. It\'s a long-term consideration. One day when I\'m available for a new partnership, you might will be there because of this site. Or we can grow this small business into something bigger because you have the required skills and you like my approach.'
+                    answer: 'To put myself up on the map, to estabilish a presence. It\'s a long-term consideration. One day when I\'m available for a new partnership, you might will be there because of this site. Or we can grow this small business into something bigger because you have the required skills and you like my approach.'
+                },
+                {
+                    question: ' the company name?',
+                    answer: 'It\'s Gábor Farkas s.p. in English, shorthand for Gábor Farkas sole proprietorship. A conventional naming in Hungary, which you will see on the invoice as well. Since honesty and transparency are cornerstones in my business policy, I\'m not comfortable with complicating the situation with a better-sounding fake name.'
+                }
+            ]
+        },
+        {
+            prefix: 'When',
+            sections: [
+                {
+                    question: ' will you be available?',
+                    answer: 'It depends. Long-term partnerships are taken extremely seriously. Estimation on the next avaiable vacancy can change on the fly based on the non-foreseeable future needs of my partners. Counseling and small side projects can be scheduled more promptly, especially during the summer, when I don\'t have any teaching duties at the university.'
+                },
+                {
+                    question: ' will you expand?',
+                    answer: 'When I find one or more potential partners to increase my capacities with. The skill requirements are unusually high, and the yield is not competitive with large enterprise positions on the same skill level. If you are still interested, contact me, and we will see if we can work something out. Please note that for me, quality always comes first, and therefore, I\'m not actively pursuing expansion.'
+                }
+            ]
+        },
+        {
+            prefix: 'Where',
+            sections: [
+                {
+                    question: ' are you based at?',
+                    answer: 'In Pécs, Hungary. Usually physical contact is not required for counseling and side projects, only for the team building aspect of long-term partnerships. However, if the demand is high enough to pay my expenses, I\'m willing to travel now and then.'
+                },
+                {
+                    question: ' are your references?',
+                    answer: 'Some of the ones I\'m allowed to show, are carefully hidden on the home page under some hexagons, if you are browsing this site using a large enough screen. If you would like to hear about the others, please contact me.'
                 }
             ]
         }
