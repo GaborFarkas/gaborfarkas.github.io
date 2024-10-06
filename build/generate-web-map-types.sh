72b0624a10f7be0b280bf32594b5132db4383e01
GaborFarkas <raandal73@gmail.com>
2024-10-06 12:38:27 +0200

    Fix ol types generation

--- build/generate-web-map-types.sh
+++ build/generate-web-map-types.sh
@@ -36,57 +36,13 @@ echo "$TYPES" >> $DTSPATH
 echo "}" >> $DTSPATH
 echo "declare var map: L.Map;" >> $DTSPATH
 
-# OpenLayers is the problematic child, we need its source code and build tools first
-rm -rf openlayers
-git clone git@github.com:openlayers/openlayers.git
-
-# Get currently installed version and checkout to its ref
-VER=$(grep VERSION node_modules/ol/util.js | cut -d "'" -f2)
-cd openlayers
-git checkout "v$VER"
-
-# Run the full build process
-npm install
-npm run build-full
-npm run generate-types
-cd build
-
-# Set up the rollup config for generating flattened d.ts files
-# Inspired by https://medium.com/@martin_hotell/typescript-library-tips-rollup-your-types-995153cc81c7
-npm install rollup-plugin-dts
-ROLLUPPATH=rollup-dts.js
-echo "import dts from 'rollup-plugin-dts';" > $ROLLUPPATH
-echo "const config = [" >> $ROLLUPPATH
-echo "  {" >> $ROLLUPPATH
-echo "    input: './ol/{FILENAME}.d.ts'," >> $ROLLUPPATH
-echo "    output: [{ file: './{FILENAME}.d.ts', format: 'es' }]," >> $ROLLUPPATH
-echo "    plugins: [dts({respectExternal: true})]" >> $ROLLUPPATH
-echo "  }" >> $ROLLUPPATH
-echo "];" >> $ROLLUPPATH
-echo "export default config;" >> $ROLLUPPATH
-
-# Use the custom rollup config to generate a flattened d.ts for every namespace including the main one
-DTSPATH=../../src/assets/web-mapping/types/openlayers.d.ts
+# OpenLayers can be rolled up by targeting its dist d.ts file with some modifications
+DTSPATH=src/assets/web-mapping/types/openlayers.d.ts
+npx rollup --config build/rollup-ol.mjs
+sed -i "s/\(^import[[:space:]]\*.*;$\)//g" $DTSPATH
+sed -i "s/\(^export[[:space:]]{[[:space:]]ol[[:space:]]as[[:space:]]default.*;$\)//g" $DTSPATH
+sed -i "s/\(^declare[[:space:]]namespace[[:space:]]ol[[:space:]]{.*$\)//g" $DTSPATH
+TYPES=$(cat $DTSPATH)
 echo "declare namespace ol {" > $DTSPATH
-ROLLUPCONFIGTEMPLATE=$(cat $ROLLUPPATH)
-sed -i 's/{FILENAME}/index/g' $ROLLUPPATH
-npx rollup --config $ROLLUPPATH
-EXPORTLINE=$(grep "export {" index.d.ts)
-FIXEDEXPORTLINE="$(echo $EXPORTLINE | cut -d '}' -f1), control events extent format geom interaction layer proj render reproj style tilegrid webgl };"
-sed -i "s/$EXPORTLINE/$FIXEDEXPORTLINE/" index.d.ts
-echo "$(cat index.d.ts)" >> $DTSPATH
-
-for NAMESPACE in control events extent format geom interaction layer proj render reproj style tilegrid webgl; do
-    echo "declare namespace $NAMESPACE {" >> $DTSPATH
-    echo "$ROLLUPCONFIGTEMPLATE" > $ROLLUPPATH
-    sed -i "s/{FILENAME}/$NAMESPACE/g" $ROLLUPPATH
-    npx rollup --config $ROLLUPPATH
-    echo "$(cat $NAMESPACE.d.ts)" >> $DTSPATH
-    echo "}" >> $DTSPATH
-done
-
-echo "}" >> $DTSPATH
+echo "$TYPES" >> $DTSPATH
 echo "declare var map: ol.Map;" >> $DTSPATH
-
-cd ../..
-rm -rf openlayers
